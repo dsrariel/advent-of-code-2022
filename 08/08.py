db46eefb8552bcc0f6abc3c449cda1a36247e46d
Ariel <dsrariel@gmail.com>
2023-03-25 20:19:05 -0300

    refactor

--- 08/08.py
+++ 08/08.py
@@ -1,62 +1,62 @@
+from collections import namedtuple
 from itertools import product
 from typing import List, Tuple, Callable
 
 FILEPATH = "08-ex.input"
 
+Tree = namedtuple("Tree", "x y")
 
-def get_tree_matrix_from_file(filepath: str):
+
+class Forest:
+    def __init__(self, tree_matrix: List[List[int]]):
+        self._matrix = tree_matrix
+        self.x = len(tree_matrix)
+        self.y = len(tree_matrix[0])
+
+    def get_tree_height(self, tree: Tree) -> int:
+        return self._matrix[tree.x][tree.y]
+
+    def _is_tree_smaller_than_any_neighbor(self, neighbors: List[Tree], tree: Tree) -> bool:
+        for neighbor in neighbors:
+            if self.get_tree_height(neighbor) >= self.get_tree_height(tree):
+                return True
+        return False
+
+    def _is_tree_left_hidden(self, tree: Tree):
+        neighbors = [Tree(tree.x, y) for y in range(0, tree.y)]
+        return self._is_tree_smaller_than_any_neighbor(neighbors, tree)
+
+    def _is_tree_right_hidden(self, tree: Tree):
+        neighbors = [Tree(tree.x, y) for y in range(tree.y + 1, self.y)]
+        return self._is_tree_smaller_than_any_neighbor(neighbors, tree)
+
+    def _is_tree_up_hidden(self, tree: Tree):
+        neighbors = [Tree(x, tree.y) for x in range(0, tree.x)]
+        return self._is_tree_smaller_than_any_neighbor(neighbors, tree)
+
+    def _is_tree_down_hidden(self, tree: Tree):
+        neighbors = [Tree(x, tree.y) for x in range(tree.x + 1, self.x)]
+        return self._is_tree_smaller_than_any_neighbor(neighbors, tree)
+
+    def is_tree_hidden(self, tree: Tree):
+        return (
+            self._is_tree_left_hidden(tree)
+            and self._is_tree_right_hidden(tree)
+            and self._is_tree_up_hidden(tree)
+            and self._is_tree_down_hidden(tree)
+        )
+
+
+def create_forest_from_file(filepath: str) -> Forest:
     with open(filepath) as f:
-        matrix = [[int(i) for i in line.rstrip()] for line in f.readlines()]
-    return matrix
-
-
-def is_border_tree(matrix: List[List[int]], tree: Tuple[int, int]):
-    matrix_x, matrix_y = len(matrix) - 1, len(matrix[0]) - 1
-    return tree[0] == 0 or tree[0] == matrix_x or tree[1] == 0 or tree[1] == matrix_y
-
-
-def is_direction_hidden(start: int, end: int, comparison: Callable[[int], bool]):
-    for i in range(start, end):
-        if comparison(i):
-            return True
-    return False
-
-
-def is_left_hidden(matrix: List[List[int]], tree: Tuple[int, int]):
-    return is_direction_hidden(0, tree[1], lambda y: matrix[tree[0]][y] >= matrix[tree[0]][tree[1]])
-
-
-def is_right_hidden(matrix: List[List[int]], tree: Tuple[int, int]):
-    return is_direction_hidden(tree[1] + 1, len(matrix[0]), lambda y: matrix[tree[0]][y] >= matrix[tree[0]][tree[1]])
-
-
-def is_up_hidden(matrix: List[List[int]], tree: Tuple[int, int]):
-    return is_direction_hidden(0, tree[0], lambda x: matrix[x][tree[1]] >= matrix[tree[0]][tree[1]])
-
-
-def is_down_hidden(matrix: List[List[int]], tree: Tuple[int, int]):
-    return is_direction_hidden(tree[0] + 1, len(matrix), lambda x: matrix[x][tree[1]] >= matrix[tree[0]][tree[1]])
-
-
-def is_tree_hidden(matrix: List[List[int]], tree: Tuple[int, int]):
-    return (
-        is_left_hidden(matrix, tree)
-        and is_right_hidden(matrix, tree)
-        and is_up_hidden(matrix, tree)
-        and is_down_hidden(matrix, tree)
-    )
-
-
-def is_tree_visible(matrix: List[List[int]], tree: Tuple[int, int]):
-    return is_border_tree(matrix, tree) or not is_tree_hidden(matrix, tree)
+        return Forest([[int(i) for i in line.rstrip()] for line in f.readlines()])
 
 
 def main():
-    matrix = get_tree_matrix_from_file(FILEPATH)
-    x, y = len(matrix), len(matrix[0])
-    for i, j in product(range(x), range(y)):
-        print(is_tree_visible(matrix, (i, j)), end="\n" if j == y - 1 else "\t")
-    visible_trees_count = sum([is_tree_visible(matrix, (i, j)) for i, j in product(range(x), range(y))])
+    forest = create_forest_from_file(FILEPATH)
+    visible_trees_count = sum(
+        [not forest.is_tree_hidden(Tree(i, j)) for i, j in product(range(forest.x), range(forest.y))]
+    )
     print("The number of visible trees in the matrix is:", visible_trees_count)
 
 
