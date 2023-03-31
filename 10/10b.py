e7750c2bb185164c80c0f3add646ebbdbd5fa371
Ariel <dsrariel@gmail.com>
2023-03-31 00:10:38 -0300

    solve part two

--- 10/10b.py
+++ 10/10b.py
@@ -3,10 +3,7 @@ from collections import namedtuple
 from dataclasses import dataclass
 from typing import Dict, List, Tuple
 
-SNAPSHOT_INTERVAL = 40
-SNAPSHOT_CEIL = 220
-
-Waveform = namedtuple("Waveform", "x cycle")
+Sprite = namedtuple("Waveform", "x cycle")
 
 
 @dataclass
@@ -14,10 +11,10 @@ class Command:
     duration: int
     action: callable
 
-    def execute(self, waveform: Waveform, parameters: Dict) -> Waveform:
-        x = self.action(waveform.x, parameters)
-        cycle = waveform.cycle + self.duration
-        return Waveform(x, cycle)
+    def execute(self, sprite: Sprite, parameters: Dict) -> Sprite:
+        x = self.action(sprite.x, parameters)
+        cycle = sprite.cycle + self.duration
+        return Sprite(x, cycle)
 
 
 commands = {
@@ -31,40 +28,33 @@ def parse_command_line(line: str) -> Tuple[Command, Dict]:
     return commands[args[0]], args[1:]
 
 
-def update_snapshots(snapshots: List[int], snapshot_cycle: int, previous: Waveform, current: Waveform) -> int:
-    wave = None
-    if current.cycle == snapshot_cycle:
-        wave = current
-    elif current.cycle > snapshot_cycle > previous.cycle:
-        wave = previous
-
-    if wave is None:
-        return snapshot_cycle
-
-    snapshots.append(wave.x * snapshot_cycle)
-    next_snapshot_cycle = snapshot_cycle + SNAPSHOT_INTERVAL
-    if next_snapshot_cycle > SNAPSHOT_CEIL:
-        raise OverflowError
-    return next_snapshot_cycle
+def update_pixels(pixels: List[str], previous: Sprite, current: Sprite):
+    for _ in range(previous.cycle, current.cycle):
+        pixel = len(pixels) % 40 + 1
+        if (previous.x + 1) >= pixel >= (previous.x - 1):
+            pixels.append("#")
+        else:
+            pixels.append(".")
 
 
-def get_signal_strengths_sum(filepath: str) -> int:
-    current_wave = Waveform(1, 1)
-    next_snapshot_cycle = 20
-    snapshots = []
+def get_pixels(filepath: str) -> int:
+    current_sprite = Sprite(2, 1)
+    pixels = []
     with open(filepath) as f:
         for line in f.readlines():
             command, parameters = parse_command_line(line)
-            new_wave = command.execute(current_wave, parameters)
-            try:
-                next_snapshot_cycle = update_snapshots(snapshots, next_snapshot_cycle, current_wave, new_wave)
-            except OverflowError:
-                break
-            current_wave = new_wave
-    return sum(snapshots)
+            new_sprite = command.execute(current_sprite, parameters)
+            update_pixels(pixels, current_sprite, new_sprite)
+            current_sprite = new_sprite
+    return pixels
+
+
+def render_pixels(pixels: List[str]) -> str:
+    pixel_lines = ["".join(pixels[i * 40 : (i + 1) * 40]) for i in range(0, 6)]
+    return "\n".join(pixel_lines)
 
 
 if __name__ == "__main__":
     filepath = sys.argv[1]
-    total = get_signal_strengths_sum(filepath)
-    print(f"The sum of these signal strengths is {total}.")
+    pixels = get_pixels(filepath)
+    print(render_pixels(pixels))
